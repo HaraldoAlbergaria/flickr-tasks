14b7074193afeb0094d8863f5a6cd60188537f3a
Haraldo Filho <haraldo.filho@gmail.com>
2019-11-21 22:00:40 -0300

    Added functions to choose the photo thumbnails sizes

--- add_gear_tags/data.py
+++ add_gear_tags/data.py
@@ -4,16 +4,20 @@ photoset_id = u'72157711713136841'
 # Camera tags
 camera_tags = {
     'CanonEOSREBELT5i': '\"DSLR\" \"APS-C\" \"Canon EOS Rebel T5i\"',
-    '': ''
+    'SamsungNXmini'   : '\"CSC\" \"1-inch\" \"Samsung NX mini\"'    ,
+    ''                : ''
 }
 
 # Lens tags
 lens_tags = {
-     'EF-S10-18mmf/4.5-5.6ISSTM': '\"Canon EF-S 10-18mm f/4.5-5.6 IS STM\"',
-     'EF-S18-55mmf/3.5-5.6ISSTM': '\"Canon EF-S 18-55mm f/3.5-5.6 IS STM\"',
-     'EF-S55-250mmf/4-5.6ISSTM' : '\"Canon EF-S 55-250mm f/4-5.6 IS STM\"' ,
-     'EF-S24mmf/2.8STM'         : '\"Canon EF-S 24mm f/2.8 STM\"'          ,
-     'EF50mmf/1.8STM'           : '\"Canon EF 50mm f/1.8 STM\"'            ,
+     'EF-S10-18mmf/4.5-5.6ISSTM': '\"Canon EF-S 10-18mm f/4.5-5.6 IS STM\"' ,
+     'EF-S18-55mmf/3.5-5.6ISSTM': '\"Canon EF-S 18-55mm f/3.5-5.6 IS STM\"' ,
+     'EF-S55-250mmf/4-5.6ISSTM' : '\"Canon EF-S 55-250mm f/4-5.6 IS STM\"'  ,
+     'EF-S24mmf/2.8STM'         : '\"Canon EF-S 24mm f/2.8 STM\"'           ,
+     'EF50mmf/1.8STM'           : '\"Canon EF 50mm f/1.8 STM\"'             ,
+     'NX-M9mmF3.5'              : '\"Samsung NX-M 9mm f/3.5 ED\"'           ,
+     'NX-M17mmF1.8OIS'          : '\"Samsung NX-M 17mm f/1.8 ED OIS\"'      ,
+     'NX-M9-27mmF3.5-5.6OIS'    : '\"Samsung NX-M 9-27mm f/3.5-5.6 ED OIS\"',
      ''                         : ''
 }
 
--- generate_kml/procs.py
+++ generate_kml/procs.py
@@ -40,24 +40,41 @@ def processPhoto(photo_id, photo_title, user_id):
     try:
         # get photo information
         photo_title = photo_title.replace('&', 'and')
-        small_size_url = flickr.photos.getSizes(api_key=api_key, user_id=user_id, photo_id=photo_id)['sizes']['size'][3]['source']
-        medium_size_url = flickr.photos.getSizes(api_key=api_key, user_id=user_id, photo_id=photo_id)['sizes']['size'][6]['source']
         photos_base_url = flickr.people.getInfo(api_key=api_key, user_id=user_id)['person']['photosurl']['_content']
         photo_url = photos_base_url + photo_id
         photo_perm = flickr.photos.getPerms(api_ky=api_key, photo_id=photo_id)['perms']['ispublic']
+        # get thumbnails urls
+        photo_sizes = flickr.photos.getSizes(api_key=api_key, user_id=user_id, photo_id=photo_id)['sizes']['size']
+        e = 1
+        m = 1
+        if(photo_sizes[e]['width']/photo_sizes[e]['height'] > 1):
+            while(photo_sizes[e]['width'] < 240 or photo_sizes[e]['height'] < 120):
+                e += 1
+            while(photo_sizes[m]['width'] < 500 or photo_sizes[m]['height'] < 250):
+                m += 1
+        else:
+            while(photo_sizes[e]['width'] < 120 or photo_sizes[e]['height'] < 240):
+                e += 1
+            while(photo_sizes[m]['width'] < 250 or photo_sizes[m]['height'] < 500):
+                m += 1
+        earth_thumb_url = photo_sizes[e]['source']
+        mymaps_thumb_url = photo_sizes[m]['source']
         # get photo location data
         location = flickr.photos.geo.getLocation(api_ky=api_key, photo_id=photo_id)
         latitude = location['photo']['location']['latitude']
         longitude = location['photo']['location']['longitude']
         geo_perm = flickr.photos.geo.getPerms(api_ky=api_key, photo_id=photo_id)['perms']['ispublic']
         # write to Google Earth's file
-        earth_file = open("/home/pi/flickr_tasks/generate_kml/my_flickr_photos.earth.kml", "a")
-        earth_file.write("        <Placemark>\n            <name>{0}</name>\n            <description><![CDATA[<a href=\"{1}\"><img src=\"{2}\" /></a>]]></description>\n            <LookAt>\n                <longitude>{3}</longitude>\n                <latitude>{4}</latitude>\n                <altitude>0</altitude>\n            </LookAt>\n            <styleUrl>#msn_placemark_circle</styleUrl>\n            <Point>\n                <gx:drawOrder>1</gx:drawOrder>\n                <coordinates>{4},{3}</coordinates>\n            </Point>\n        </Placemark>\n".format(photo_title, photo_url, small_size_url, latitude, longitude))
-        earth_file.close()
+        if not hasTag(photo_id, not_map_tag):
+            earth_file = open("/home/pi/flickr_tasks/generate_kml/my_flickr_photos.earth.kml", "a")
+            earth_file.write("        <Placemark>\n            <name>{0}</name>\n            <description><![CDATA[<a href=\"{1}\"><img src=\"{2}\" /></a>]]></description>\n            <LookAt>\n                <longitude>{3}</longitude>\n                <latitude>{4}</latitude>\n                <altitude>0</altitude>\n            </LookAt>\n            <styleUrl>#msn_placemark_circle</styleUrl>\n            <Point>\n                <gx:drawOrder>1</gx:drawOrder>\n                <coordinates>{4},{3}</coordinates>\n            </Point>\n        </Placemark>\n".format(photo_title, photo_url, earth_thumb_url, latitude, longitude))
+            earth_file.close()
+            print("Added marker to 'Google Earth'!")
         # write to Google My Maps' file if photo and location are public
         if photo_perm == 1 and geo_perm == 1 and not hasTag(photo_id, not_map_tag):
             mymaps_file = open("/home/pi/flickr_tasks/generate_kml/my_flickr_photos.mymaps.kml", "a")
-            mymaps_file.write("        <Placemark>\n            <name>{0}</name>\n            <description><![CDATA[<img src=\"{1}\" />{2}]]></description>\n            <LookAt>\n                <longitude>{3}</longitude>\n                <latitude>{4}</latitude>\n                <altitude>0</altitude>\n            </LookAt>\n            <styleUrl>#icon-1535-0288D1</styleUrl>\n            <Point>\n                <gx:drawOrder>1</gx:drawOrder>\n                <coordinates>{4},{3}</coordinates>\n            </Point>\n        </Placemark>\n".format(photo_title, medium_size_url, photo_url, latitude, longitude))
+            mymaps_file.write("        <Placemark>\n            <name>{0}</name>\n            <description><![CDATA[<img src=\"{1}\" />{2}]]></description>\n            <LookAt>\n                <longitude>{3}</longitude>\n                <latitude>{4}</latitude>\n                <altitude>0</altitude>\n            </LookAt>\n            <styleUrl>#icon-1535-0288D1</styleUrl>\n            <Point>\n                <gx:drawOrder>1</gx:drawOrder>\n                <coordinates>{4},{3}</coordinates>\n            </Point>\n        </Placemark>\n".format(photo_title, mymaps_thumb_url, photo_url, latitude, longitude))
             mymaps_file.close()
+            print("Added marker to 'Google My Maps!'")
     except:
         pass
