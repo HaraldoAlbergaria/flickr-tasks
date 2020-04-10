f1958965a8e2f927fcd7292148ee94977c188445
Haraldo Filho <haraldo.filho@gmail.com>
2020-04-09 22:57:00 -0300

    Edited 'data.py' in 'auto_add2groups'

--- auto_add2groups/auto_add2group_efs250/data.py
+++ auto_add2groups/auto_add2group_efs250/data.py
@@ -1,6 +1,7 @@
 # Canon EF-S @250mm
 group_url = 'https://www.flickr.com/groups/efs-at250mm/'
 group_limit = 6
+group_full = False
 
 lens_models = ( 'EF-S55-250mm f/4-5.6 IS STM',
                 'EF-S55-250mm f/4-5.6 IS II',
@@ -13,6 +14,3 @@ lens_models = ( 'EF-S55-250mm f/4-5.6 IS STM',
                 'Canon EF-S 55-250mm f/4-5.6 IS' )
 
 focal_lengths = ( '250.0 mm' )
-
-set_250mm_id = '72157666561896128'
-group_full = False
