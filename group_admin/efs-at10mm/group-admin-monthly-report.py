1e6e92055977b2fd7d6b52f18d0e7f6e139229ea
Haraldo Filho <haraldo.filho@gmail.com>
2020-07-15 22:47:15 -0300

    Fixed output url of group-admin-montly-report.py

--- group_admin/efs-at10mm/group-admin-monthly-report.py
+++ group_admin/efs-at10mm/group-admin-monthly-report.py
@@ -54,7 +54,7 @@ photos_per_page = int(pool['photos']['perpage'])
 # set output files names
 report_file_name = '/home/pi/flickr_tasks/group_admin/{0}/{1}.photos.admin.monthly.txt'.format(group_alias, group_name).replace(' ','_')
 remove_file_name = '/home/pi/flickr_tasks/group_admin/{0}/remove-photos.py'.format(group_alias)
-html_file_name   = '/home/pi/github/pages/flickr-group-admin-reports/{0}.monthly.html'.format(group_name).replace(' ','_')
+html_file_name   = '/home/pi/github/hpfilho.github.io/reports/{0}.monthly.html'.format(group_name).replace(' ','_')
 
 # create and add header to report file
 procs.addReportHeader(report_file_name, html_file_name, group_name, total_of_photos)
